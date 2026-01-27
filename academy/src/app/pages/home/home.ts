f856ee8d40f117bade817c4e48f4cfc047310920
Igor lima <igor_purosso@hotmail.com>
2026-01-27 00:17:02 -0300

    Initial commit

---
+++ academy/src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Landing } from '../../components/landing/landing';
+
+@Component({
+  selector: 'app-home',
+  imports: [Landing],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
